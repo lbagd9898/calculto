338b940266c68a905bc1dc8c78c4323f2fa41553
Lydia Bagdon <74695306+lbagd9898@users.noreply.github.com>
2024-10-10 18:02:25 -0400

    basic calculator layout finished and a few basic functions set

---
+++ javascript.js
@@ -0,0 +1,25 @@
+const a = ''
+const operator = ''
+const b = ''
+
+function add(a, b) {
+    return a + b
+}
+
+function subtract(a, b) {
+    return a - b
+}
+
+function multiply(a, b) {
+    return a * b
+}
+
+function divide(a, b) {
+    return a / b
+}
+
+function operate (a, b, operator) {
+    return operator(a, b)
+}
+
+
